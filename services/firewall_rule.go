b3313d56cbced6eb4b06b063e399ea7724226e52
Quentin Lemaire <quentin@lemairepro.fr>
2020-04-01 12:29:03 +0200

    Add some debug logs when interacting with firewall rules

--- services/firewall_rule.go
+++ services/firewall_rule.go
@@ -11,7 +11,11 @@ import (
 
 // ListFirewallRule returns a set of firewall rules related to an application
 func ListFirewallRule(manager models.FirewallRuleManager, project, serviceProject, application string) (*models.ApplicationRule, error) {
-	logrus.Debugf("Manager will list rules for project %s\n", project)
+	logrus.WithFields(logrus.Fields{
+		"project":         project,
+		"service_project": serviceProject,
+		"application":     application,
+	}).Debugln("Listing rules")
 
 	// List all firewall rule in given project
 	gRules, err := manager.ListFirewallRule(project)
@@ -42,13 +46,28 @@ func ListFirewallRule(manager models.FirewallRuleManager, project, serviceProjec
 	}
 
 	endUserResult.Rules = endUserResultRules
+	logrus.WithFields(logrus.Fields{
+		"project":         project,
+		"service_project": serviceProject,
+		"application":     application,
+	}).Debugf("Found %d rules", len(endUserResultRules))
 	return &endUserResult, nil
 }
 
 // CreateFirewallRule create given firewall rule on given project
 func CreateFirewallRule(manager models.FirewallRuleManager, project string, serviceProject string, application string, ruleName string, rule compute.Firewall) (*models.ApplicationRule, error) {
-	rule.Name = fmt.Sprintf("%s-%s-%s", serviceProject, application, ruleName)
-	logrus.Debugf("Manager will create %s on %s\n", rule.Name, project)
+	customNameAndTargetTag := fmt.Sprintf("%s-%s-%s", serviceProject, application, ruleName)
+	rule.Name = customNameAndTargetTag
+	rule.TargetTags = []string{customNameAndTargetTag}
+
+	logrus.WithFields(logrus.Fields{
+		"project":         project,
+		"service_project": serviceProject,
+		"application":     application,
+		"rule_name":       customNameAndTargetTag,
+		"target_tag":      customNameAndTargetTag,
+	}).Debugln("Creating rule")
+
 	gRule, err := manager.CreateFirewallRule(project, &rule)
 	if err != nil {
 		return nil, err
@@ -70,12 +89,26 @@ func CreateFirewallRule(manager models.FirewallRuleManager, project string, serv
 
 // GetFirewallRule return matching firewall rule
 func GetFirewallRule(manager models.FirewallRuleManager, project string, serviceProject string, application string, ruleName string) (*models.ApplicationRule, error) {
-	logrus.Debugf("Searching rule mathing project '%s', service project '%s', application '%s' and name '%s'", project, serviceProject, application, ruleName)
 	n := fmt.Sprintf("%s-%s-%s", serviceProject, application, ruleName)
+
+	logrus.WithFields(logrus.Fields{
+		"project":         project,
+		"service_project": serviceProject,
+		"application":     application,
+		"rule_name":       n,
+	}).Debugln("Searching rule")
+
 	gRule, err := manager.GetFirewallRule(project, n)
 	if err != nil {
 		return nil, err
 	}
+
+	logrus.WithFields(logrus.Fields{
+		"project":         project,
+		"service_project": serviceProject,
+		"application":     application,
+		"rule_name":       n,
+	}).Debugf("Rule found with ID %d", gRule.Id)
 
 	prefix := fmt.Sprintf("%s-%s-", serviceProject, application)
 	createdRule := models.FirewallRule{
@@ -94,6 +127,12 @@ func GetFirewallRule(manager models.FirewallRuleManager, project string, service
 // DeleteFirewallRule delete firewall rule mathing project, service project, application name and rule name
 func DeleteFirewallRule(manager models.FirewallRuleManager, project, serviceProject, application, customName string) error {
 	ruleName := fmt.Sprintf("%s-%s-%s", serviceProject, application, customName)
-	logrus.Debugf("Manager will delete %s on %s.\n", ruleName, project)
+	logrus.WithFields(logrus.Fields{
+		"project":         project,
+		"service_project": serviceProject,
+		"application":     application,
+		"rule_name":       ruleName,
+	}).Debugln("Deleting rule")
+
 	return manager.DeleteFirewallRule(project, ruleName)
 }
