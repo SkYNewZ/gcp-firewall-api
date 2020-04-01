892414997c543eed27456c6fd6cb11e508a6428f
Quentin Lemaire <quentin@lemairepro.fr>
2020-04-01 12:22:55 +0200

    Create custom errors type

--- models/errors.go
+++ models/errors.go
@@ -3,41 +3,91 @@ package models
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"google.golang.org/api/googleapi"
 )
 
-// GoogleApplicationError describe a custom error response
-type GoogleApplicationError struct {
+// ApplicationError describe a global application error
+type ApplicationError struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
 }
 
-// NewGoogleApplicationError GoogleApplicationError constructor
-func NewGoogleApplicationError(err *googleapi.Error) *GoogleApplicationError {
-	return &GoogleApplicationError{
+// JSON return ApplicationError as JSON
+func (e *ApplicationError) Error() string {
+	res, _ := json.Marshal(e)
+	return string(res)
+}
+
+// NewGoogleApplicationError describe a http error response from Google
+func NewGoogleApplicationError(err *googleapi.Error) *ApplicationError {
+	return &ApplicationError{
 		Code:    err.Code,
-		Message: err.Message,
+		Message: fmt.Sprintf("Google error: %s", err.Message),
 	}
 }
 
-func (g *GoogleApplicationError) Error() string {
-	return fmt.Sprintf("Error from Google: Code %d Message '%s'", g.Code, g.Message)
+// NewBadTokenError describe a http error when decoding JWT
+func NewBadTokenError(message ...string) *ApplicationError {
+	e := NewBadRequestError()
+	e.Message = "Invalid Bearer token. Please make sure you are using 'gcloud auth print-identity-token'"
+
+	if len(message) > 0 {
+		e.Message = fmt.Sprintf("%s: %s", e.Message, message[0])
+	}
+
+	return e
 }
 
-// JSON return error as JSON format
-func (g *GoogleApplicationError) JSON() string {
-	res, _ := json.Marshal(g)
-	return string(res)
+// NewForbiddenError describe a http error response 403 Forbidden
+func NewForbiddenError(message ...string) *ApplicationError {
+	e := &ApplicationError{
+		Code:    http.StatusForbidden,
+		Message: http.StatusText(http.StatusForbidden),
+	}
+
+	if len(message) > 0 {
+		e.Message = message[0]
+	}
+
+	return e
 }
 
-// RouterError describe a generic error response
-type RouterError struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
+// NewInternalError describe a http error response 500 Internal Server Error
+func NewInternalError() *ApplicationError {
+	return &ApplicationError{
+		Code:    http.StatusInternalServerError,
+		Message: http.StatusText(http.StatusInternalServerError),
+	}
 }
 
-func (e *RouterError) Error() string {
-	res, _ := json.Marshal(e)
-	return string(res)
+// NewMethodNotAllowedError describe a http error response 405 Method Not Allowed
+func NewMethodNotAllowedError() *ApplicationError {
+	return &ApplicationError{
+		Code:    http.StatusMethodNotAllowed,
+		Message: http.StatusText(http.StatusMethodNotAllowed),
+	}
+}
+
+// NewNotFoundError describe a http error response 404 Not Found
+func NewNotFoundError() *ApplicationError {
+	return &ApplicationError{
+		Code:    http.StatusNotFound,
+		Message: http.StatusText(http.StatusNotFound),
+	}
+}
+
+// NewBadRequestError describe a http error response 400 Bad Request
+func NewBadRequestError(message ...string) *ApplicationError {
+	e := &ApplicationError{
+		Code:    http.StatusBadRequest,
+		Message: http.StatusText(http.StatusBadRequest),
+	}
+
+	if len(message) > 0 {
+		e.Message = message[0]
+	}
+
+	return e
 }
--- models/errors_test.go
+++ models/errors_test.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"fmt"
+	"net/http"
 	"testing"
 
 	"google.golang.org/api/googleapi"
@@ -14,13 +15,13 @@ type TestCase struct {
 }
 
 func TestGoogleApplicationError(t *testing.T) {
-	expectedError := *&googleapi.Error{
+	expectedError := &googleapi.Error{
 		Code:    1234,
 		Message: "dummy",
 	}
 
 	// Execute function
-	testedError := NewGoogleApplicationError(&expectedError)
+	testedError := NewGoogleApplicationError(expectedError)
 
 	suite := []TestCase{
 		TestCase{
@@ -34,19 +35,14 @@ func TestGoogleApplicationError(t *testing.T) {
 			Got:      testedError.Code,
 		},
 		TestCase{
-			Title:    "Error messaage shoud be identical",
-			Expected: expectedError.Message,
+			Title:    "Error message shoud be identical",
+			Expected: fmt.Sprintf("Google error: %s", expectedError.Message),
 			Got:      testedError.Message,
 		},
 		TestCase{
-			Title:    "Error() method should return message as fromatted string",
-			Expected: fmt.Sprintf("Error from Google: Code %d Message '%s'", expectedError.Code, expectedError.Message),
+			Title:    "Error() method should return error as JSON",
+			Expected: fmt.Sprintf(`{"code":%d,"message":"Google error: %s"}`, expectedError.Code, expectedError.Message),
 			Got:      testedError.Error(),
-		},
-		TestCase{
-			Title:    "JSON() method should return error as JSON",
-			Expected: `{"code":1234,"message":"dummy"}`,
-			Got:      testedError.JSON(),
 		},
 	}
 
@@ -54,7 +50,337 @@ func TestGoogleApplicationError(t *testing.T) {
 	for _, suiteCase := range suite {
 		t.Run(suiteCase.Title, func(t *testing.T) {
 			if suiteCase.Expected != suiteCase.Got {
-				t.Errorf("Got %d want %d", suiteCase.Got, suiteCase.Expected)
+				t.Errorf("Got '%v' want '%v'", suiteCase.Got, suiteCase.Expected)
+			}
+		})
+	}
+}
+
+func TestBadTokenError(t *testing.T) {
+	expectedError1 := ApplicationError{
+		Code:    http.StatusBadRequest,
+		Message: "Invalid Bearer token. Please make sure you are using 'gcloud auth print-identity-token'",
+	}
+
+	expectedError2 := ApplicationError{
+		Code:    http.StatusBadRequest,
+		Message: "Invalid Bearer token. Please make sure you are using 'gcloud auth print-identity-token': foo",
+	}
+
+	// Execute function
+	testedError1 := NewBadTokenError()
+	testedError2 := NewBadTokenError("foo")
+
+	suite := []TestCase{
+		TestCase{
+			Title:    "Error should not be nil",
+			Expected: false,
+			Got:      testedError1 == nil,
+		},
+		TestCase{
+			Title:    "Error should not be nil",
+			Expected: false,
+			Got:      testedError2 == nil,
+		},
+		TestCase{
+			Title:    "Error code should be identical",
+			Expected: expectedError1.Code,
+			Got:      testedError1.Code,
+		},
+		TestCase{
+			Title:    "Error code should be identical",
+			Expected: expectedError2.Code,
+			Got:      testedError2.Code,
+		},
+		TestCase{
+			Title:    "Error message shoud be identical",
+			Expected: expectedError1.Message,
+			Got:      testedError1.Message,
+		},
+		TestCase{
+			Title:    "Error message shoud be identical",
+			Expected: expectedError2.Message,
+			Got:      testedError2.Message,
+		},
+		TestCase{
+			Title:    "Error() method should return error as JSON",
+			Expected: fmt.Sprintf(`{"code":%d,"message":"%s"}`, expectedError1.Code, expectedError1.Message),
+			Got:      testedError1.Error(),
+		},
+		TestCase{
+			Title:    "Error() method should return error as JSON",
+			Expected: fmt.Sprintf(`{"code":%d,"message":"%s"}`, expectedError2.Code, expectedError2.Message),
+			Got:      testedError2.Error(),
+		},
+	}
+
+	// Launch test
+	for _, suiteCase := range suite {
+		t.Run(suiteCase.Title, func(t *testing.T) {
+			if suiteCase.Expected != suiteCase.Got {
+				t.Errorf("Got '%v' want '%v'", suiteCase.Got, suiteCase.Expected)
+			}
+		})
+	}
+}
+
+func TestForbiddenError(t *testing.T) {
+	expectedError1 := ApplicationError{
+		Code:    http.StatusForbidden,
+		Message: http.StatusText(http.StatusForbidden),
+	}
+
+	expectedError2 := ApplicationError{
+		Code:    http.StatusForbidden,
+		Message: "foo",
+	}
+
+	// Execute function
+	testedError1 := NewForbiddenError()
+	testedError2 := NewForbiddenError("foo")
+
+	suite := []TestCase{
+		TestCase{
+			Title:    "Error should not be nil",
+			Expected: false,
+			Got:      testedError1 == nil,
+		},
+		TestCase{
+			Title:    "Error should not be nil",
+			Expected: false,
+			Got:      testedError2 == nil,
+		},
+		TestCase{
+			Title:    "Error code should be identical",
+			Expected: expectedError1.Code,
+			Got:      testedError1.Code,
+		},
+		TestCase{
+			Title:    "Error code should be identical",
+			Expected: expectedError2.Code,
+			Got:      testedError2.Code,
+		},
+		TestCase{
+			Title:    "Error message shoud be identical",
+			Expected: expectedError1.Message,
+			Got:      testedError1.Message,
+		},
+		TestCase{
+			Title:    "Error message shoud be identical",
+			Expected: expectedError2.Message,
+			Got:      testedError2.Message,
+		},
+		TestCase{
+			Title:    "Error() method should return error as JSON",
+			Expected: fmt.Sprintf(`{"code":%d,"message":"%s"}`, expectedError1.Code, expectedError1.Message),
+			Got:      testedError1.Error(),
+		},
+		TestCase{
+			Title:    "Error() method should return error as JSON",
+			Expected: fmt.Sprintf(`{"code":%d,"message":"%s"}`, expectedError2.Code, expectedError2.Message),
+			Got:      testedError2.Error(),
+		},
+	}
+
+	// Launch test
+	for _, suiteCase := range suite {
+		t.Run(suiteCase.Title, func(t *testing.T) {
+			if suiteCase.Expected != suiteCase.Got {
+				t.Errorf("Got '%v' want '%v'", suiteCase.Got, suiteCase.Expected)
+			}
+		})
+	}
+}
+
+func TestNewInternalError(t *testing.T) {
+	expectedError := ApplicationError{
+		Code:    http.StatusInternalServerError,
+		Message: http.StatusText(http.StatusInternalServerError),
+	}
+
+	// Execute function
+	testedError := NewInternalError()
+
+	suite := []TestCase{
+		TestCase{
+			Title:    "Error should not be nil",
+			Expected: false,
+			Got:      testedError == nil,
+		},
+		TestCase{
+			Title:    "Error code should be identical",
+			Expected: expectedError.Code,
+			Got:      testedError.Code,
+		},
+		TestCase{
+			Title:    "Error message shoud be identical",
+			Expected: expectedError.Message,
+			Got:      testedError.Message,
+		},
+		TestCase{
+			Title:    "Error() method should return error as JSON",
+			Expected: fmt.Sprintf(`{"code":%d,"message":"%s"}`, expectedError.Code, expectedError.Message),
+			Got:      testedError.Error(),
+		},
+	}
+
+	// Launch test
+	for _, suiteCase := range suite {
+		t.Run(suiteCase.Title, func(t *testing.T) {
+			if suiteCase.Expected != suiteCase.Got {
+				t.Errorf("Got '%v' want '%v'", suiteCase.Got, suiteCase.Expected)
+			}
+		})
+	}
+}
+
+func TestNewMethodNotAllowedError(t *testing.T) {
+	expectedError := ApplicationError{
+		Code:    http.StatusMethodNotAllowed,
+		Message: http.StatusText(http.StatusMethodNotAllowed),
+	}
+
+	// Execute function
+	testedError := NewMethodNotAllowedError()
+
+	suite := []TestCase{
+		TestCase{
+			Title:    "Error should not be nil",
+			Expected: false,
+			Got:      testedError == nil,
+		},
+		TestCase{
+			Title:    "Error code should be identical",
+			Expected: expectedError.Code,
+			Got:      testedError.Code,
+		},
+		TestCase{
+			Title:    "Error message shoud be identical",
+			Expected: expectedError.Message,
+			Got:      testedError.Message,
+		},
+		TestCase{
+			Title:    "Error() method should return error as JSON",
+			Expected: fmt.Sprintf(`{"code":%d,"message":"%s"}`, expectedError.Code, expectedError.Message),
+			Got:      testedError.Error(),
+		},
+	}
+
+	// Launch test
+	for _, suiteCase := range suite {
+		t.Run(suiteCase.Title, func(t *testing.T) {
+			if suiteCase.Expected != suiteCase.Got {
+				t.Errorf("Got '%v' want '%v'", suiteCase.Got, suiteCase.Expected)
+			}
+		})
+	}
+}
+
+func TestNewNotFoundError(t *testing.T) {
+	expectedError := ApplicationError{
+		Code:    http.StatusNotFound,
+		Message: http.StatusText(http.StatusNotFound),
+	}
+
+	// Execute function
+	testedError := NewNotFoundError()
+
+	suite := []TestCase{
+		TestCase{
+			Title:    "Error should not be nil",
+			Expected: false,
+			Got:      testedError == nil,
+		},
+		TestCase{
+			Title:    "Error code should be identical",
+			Expected: expectedError.Code,
+			Got:      testedError.Code,
+		},
+		TestCase{
+			Title:    "Error message shoud be identical",
+			Expected: expectedError.Message,
+			Got:      testedError.Message,
+		},
+		TestCase{
+			Title:    "Error() method should return error as JSON",
+			Expected: fmt.Sprintf(`{"code":%d,"message":"%s"}`, expectedError.Code, expectedError.Message),
+			Got:      testedError.Error(),
+		},
+	}
+
+	// Launch test
+	for _, suiteCase := range suite {
+		t.Run(suiteCase.Title, func(t *testing.T) {
+			if suiteCase.Expected != suiteCase.Got {
+				t.Errorf("Got '%v' want '%v'", suiteCase.Got, suiteCase.Expected)
+			}
+		})
+	}
+}
+
+func TestNewBadRequestError(t *testing.T) {
+	expectedError1 := ApplicationError{
+		Code:    http.StatusBadRequest,
+		Message: http.StatusText(http.StatusBadRequest),
+	}
+
+	expectedError2 := ApplicationError{
+		Code:    http.StatusBadRequest,
+		Message: "foo",
+	}
+
+	// Execute function
+	testedError1 := NewBadRequestError()
+	testedError2 := NewBadRequestError("foo")
+
+	suite := []TestCase{
+		TestCase{
+			Title:    "Error should not be nil",
+			Expected: false,
+			Got:      testedError1 == nil,
+		},
+		TestCase{
+			Title:    "Error should not be nil",
+			Expected: false,
+			Got:      testedError2 == nil,
+		},
+		TestCase{
+			Title:    "Error code should be identical",
+			Expected: expectedError1.Code,
+			Got:      testedError1.Code,
+		},
+		TestCase{
+			Title:    "Error code should be identical",
+			Expected: expectedError2.Code,
+			Got:      testedError2.Code,
+		},
+		TestCase{
+			Title:    "Error message shoud be identical",
+			Expected: expectedError1.Message,
+			Got:      testedError1.Message,
+		},
+		TestCase{
+			Title:    "Error message shoud be identical",
+			Expected: expectedError2.Message,
+			Got:      testedError2.Message,
+		},
+		TestCase{
+			Title:    "Error() method should return error as JSON",
+			Expected: fmt.Sprintf(`{"code":%d,"message":"%s"}`, expectedError1.Code, expectedError1.Message),
+			Got:      testedError1.Error(),
+		},
+		TestCase{
+			Title:    "Error() method should return error as JSON",
+			Expected: fmt.Sprintf(`{"code":%d,"message":"%s"}`, expectedError2.Code, expectedError2.Message),
+			Got:      testedError2.Error(),
+		},
+	}
+
+	// Launch test
+	for _, suiteCase := range suite {
+		t.Run(suiteCase.Title, func(t *testing.T) {
+			if suiteCase.Expected != suiteCase.Got {
+				t.Errorf("Got '%v' want '%v'", suiteCase.Got, suiteCase.Expected)
 			}
 		})
 	}
